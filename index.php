61d16a7fe0da6165fc94817f66c33ee421ff8fc3
Anzelmus <160823300+Anzelmus@users.noreply.github.com>
2026-03-04 07:42:59 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,27 @@
 <?php
-function get_data($data){
-    if (isset($_GET[$data])) {
-        return $_GET[$data];
-    } else {
-        return null;
-    }
-}
+$koneksi = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "12_pplg1"
+);
+echo var_dump($koneksi)
+// var_dump melihat isi dari varialbel yang sudsh dbuat we
+
+ 
 ?>
 
-<!DOCTYPE html>
 <html>
-<head>
-    <title>Cek Nilai USK</title>
-</head>
-<body>
-
-<h1>
-    <?php 
-    echo get_data('nama') ?? 'Nama belum diisi'; 
-    ?>
-</h1>
-
-<h1>
-<?php
-$nilai = get_data('nilai');
-
-if ($nilai !== null) {
-    if ($nilai >= 78) {
-        echo "Anda lulus! = $nilai";
-    } else {
-        echo "Anda tidak lulus! = $nilai";
-    }
-} else {
-    echo "Nilai belum diisi";
-}
-?>
-</h1>
-
-<hr>
-
-<form action="" method="get">
-    <label>Nama</label>
-    <input type="text" name="nama">
-    <br>
-
-    <label>Nilai</label>
-    <input type="number" name="nilai">
-    <br>
-
-    <input type="submit" value="Cek Nilai">
-</form>
-
-</body>
+    <head>
+        <title>
+            php oop
+        </title>
+    </head>
+    <body>
+        <h1>koneksi</h1>
+        <?php echo $koneksi?>
+        <br>
+        <?= $koneksi?>
+        <hr>
+    </body>
 </html>
